6cc2663ff3a623c01d1901ff72ef0f65c853a69d
chuckskullgreen <58377920+chuckskullgreen@users.noreply.github.com>
2019-12-05 00:25:30 +0100

    Add files via upload

---
+++ JS HOMEWORK 1/js/script.js
@@ -0,0 +1,5 @@
+let phones = 30;
+let singlePrice = 119.95;
+let tax = phones * singlePrice * .05;
+let sum = phones * singlePrice + tax;
+console.log("The price of ", phones, "phones with single price ($", singlePrice, ") with total tax 5%:", tax, ", the total value is: ", sum);
